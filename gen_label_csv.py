f3377ea5592e228b436fca8af53e7075b02bab91
Andyyu001 <32827452+Andyyu001@users.noreply.github.com>
2021-11-17 09:46:02 +0800

    Update gen_label_csv.py

--- gen_label_csv.py
+++ gen_label_csv.py
@@ -9,36 +9,7 @@ label_warp = {'norain': 0,
               'rain': 1,
               }
 
-# train data- real-world
-# data_path = 'data/Dif_real_rain'
-# img_path, label = [], []
-#
-# for first_path in os.listdir(data_path):
-#     first_path = osp.join(data_path, first_path)
-#
-#     if 'norain' in first_path:
-#         i = 0
-#         for img in os.listdir(first_path):
-#             if i < 5000:
-#                 img_path.append(osp.join(first_path, img))
-#                 label.append('norain')
-#                 i = i + 1
-#                 print('i',i)
-#     elif 'rain' in first_path:
-#         k = 0
-#         for img in os.listdir(first_path):
-#             if k < 5000:
-#                 img_path.append(osp.join(first_path, img))
-#                 label.append('rain')
-#                 k = k + 1
-#                 print('k', k)
-#
-# label_file = pd.DataFrame({'img_path': img_path, 'label': label})
-# label_file['label'] = label_file['label'].map(label_warp)
-#
-# label_file.to_csv('data/realrainlabel5000.csv', index=False)
-
-data_path = 'data/DID_spe/train'
+data_path = 'data/train'
 img_path, label = [], []
 
 for first_path in os.listdir(data_path):
@@ -59,18 +30,3 @@ label_file = pd.DataFrame({'img_path': img_path, 'label': label})
 label_file['label'] = label_file['label'].map(label_warp)
 
 label_file.to_csv('data/rainDIDlabel.csv', index=False)
-
-# test data
-# test_data_path = 'data/test_derain'
-# all_test_img = os.listdir(test_data_path)
-# test_img_path = []
-#
-# for fpath in all_test_img:
-#     fpath = osp.join(test_data_path, fpath)
-#
-#     for img in os.listdir(fpath):
-#         if '.png' in img:
-#             test_img_path.append(osp.join(fpath, img))
-#
-# test_file = pd.DataFrame({'img_path': test_img_path})
-# test_file.to_csv('data/raintest.csv', index=False)
